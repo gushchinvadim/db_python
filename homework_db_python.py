55eb498055ef16b740a93a630d24d557b97097ad
Vadim Gushchin <gushchinvadim@gmail.com>
2025-03-12 12:24:44 +0300

    second commit

--- homework_db_python.py
+++ homework_db_python.py
@@ -1,97 +1,107 @@
 
 import psycopg2
+class Database:
+    def __init__(self):
+        self.connection = psycopg2.connect(database="postgres", user="postgres", password="postgres")
+        self.cursor = self.connection.cursor()
+        self.create_table()
 
-def create_db(conn):
-    with conn.cursor() as cur:
-        cur.execute("""
-            CREATE TABLE IF NOT EXISTS client(
-            id SERIAL PRIMARY KEY,
-            first_name VARCHAR(40) NOT NULL,
-            last_name VARCHAR(40) NOT NULL,
-            email VARCHAR(40) NOT NULL,
-            phones DECIMAL
-            );
-            """)
-    with conn.cursor() as cur:
-        cur.execute("""
-            CREATE TABLE IF NOT EXISTS client_phone(
-            client_id INTEGER NOT NULL REFERENCES client(id),
-            phones DECIMAL
-            );
-            """)
-        conn.commit()
+    def create_table(self):
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS clients (
+                id SERIAL PRIMARY KEY,
+                first_name TEXT NOT NULL,
+                last_name TEXT NOT NULL,
+                email TEXT NOT NULL UNIQUE
+            )
+        ''')
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS phones (
+                id SERIAL PRIMARY KEY,
+                client_id INTEGER,
+                phone_number TEXT,
+                FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE
+            )
+        ''')
+        self.connection.commit()
 
-def add_client(conn, first_name, last_name, email, phones=None):
-    with conn.cursor() as cur:
-        cur.execute("""
-            INSERT INTO client(first_name, last_name, email, phones)
-            VALUES (%s,%s,%s,%s);""",
-            (first_name, last_name, email, phones))
-        conn.commit()
-#
-def add_phone(conn, client_id, phone):
-    with conn.cursor() as cur:
-        cur.execute("""
-            INSERT INTO client_phone(client_id, phones)
-            VALUES (%s,%s);""",
-            (client_id, phone))
-        conn.commit()
-def change_client(conn, id, first_name=None, last_name=None, email=None, phones=None):
-    with conn.cursor() as cur:
-        cur.execute("""
-            UPDATE client SET first_name=%s, last_name=%s, email=%s, phones=%s WHERE id=%s;
-            """, (first_name,last_name, email,phones, id))
-        cur.execute("""
-            SELECT * FROM client;
-            """)
-        print(cur.fetchall())
-        conn.commit()
+    def add_client(self, first_name, last_name, email=None):
+        self.cursor.execute('''
+            INSERT INTO clients (first_name, last_name, email) VALUES (%s, %s, %s)
+        ''', (first_name, last_name, email))
+        self.connection.commit()
 
-def delete_phone(conn, client_id, phone):
-    with conn.cursor() as cur:
-        cur.execute("""
-            DELETE FROM client_phone WHERE client_id=%s and phones=%s;
-            """, (client_id, phone))
-        conn.commit()
+    def add_phone(self, client_id, phone_number):
+        self.cursor.execute('''
+            INSERT INTO phones (client_id, phone_number) VALUES (%s, %s)
+        ''', (client_id, phone_number))
+        self.connection.commit()
 
-def delete_client(conn, client_id, id):
-    with conn.cursor() as cur:
-        cur.execute("""
-            DELETE FROM client_phone WHERE client_id=%s;
-            """, (client_id,))
-        cur.execute("""
-            DELETE FROM client WHERE id=%s;
-            """, (id,))
-        cur.execute("""
-            SELECT * FROM client;
-            """)
-        print(cur.fetchall())
+    def update_client(self, client_id, first_name, last_name, email=None):
+        self.cursor.execute('''
+            UPDATE clients SET first_name = %s, last_name = %s, email = %s WHERE id = %s
+        ''', (first_name, last_name, email, client_id))
+        self.connection.commit()
 
-def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
-    with conn.cursor() as cur:
-        cur.execute("""
-        SELECT first_name, last_name FROM client WHERE first_name = %s or last_name = %s or email = %s ;
-        """, (first_name,last_name, email))
+    def delete_phone(self, client_id, phone_id):
+        self.cursor.execute('''
+            DELETE FROM phones WHERE id = %s AND client_id = %s
+        ''', (phone_id, client_id))
+        self.connection.commit()
 
-        print(cur.fetchall())
+    def delete_client(self, client_id):
+        self.cursor.execute('''
+            DELETE FROM clients WHERE id = %s
+        ''', (client_id,))
+        self.connection.commit()
 
-with psycopg2.connect(database="postgres", user="postgres", password="postgres") as conn:
+    def find_client(self, search_term):
+        self.cursor.execute('''
+            SELECT clients.id, first_name, last_name, email FROM clients
+            WHERE first_name LIKE %s OR last_name LIKE %s OR email LIKE %s
+        ''', (f'%{search_term}%', f'%{search_term}%', f'%{search_term}%'))
+        return self.cursor.fetchall()
 
-    # create_db(conn)
-    # add_client(conn,'Иван','Солярка', 'ivsol@test.com', )
-    # add_client(conn, 'Иван', 'Иванов', 'ivaiva@test.com', 98753654422)
-    # add_client(conn, 'Петр', 'Петров', 'petrp@test.com', 44473654422)
-    # add_phone(conn,1,44473654422)
-    # add_phone(conn, 2, 98753654422)
-    # add_phone(conn, 3, 89456780987)
-    # change_client(conn,1,'Gena','Gurin','gena@kjh.com')
-    # change_client(conn, 2, 'Vasja', 'Pupkin', 'vpuk@kjh.com', 8937636366)
-    # delete_phone(conn, 2, 98753654422)
-    # delete_phone(conn, 1, 44473654422)
-    # delete_phone(conn, 3, 89456780987)
-    # delete_client(conn,1, 1)
-    # delete_client(conn,2, 2)
-    # find_client(conn,None,None,'ivaiva@test.com')
-    # find_client(conn, 'Петр', None, None)
+    def get_client_phones(self, client_id):
+        self.cursor.execute('''
+            SELECT phone_number FROM phones WHERE client_id = %s
+        ''', (client_id,))
+        return self.cursor.fetchall()
 
-conn.close()
+    def close(self):
+        self.connection.close()
+
+# Пример использования
+if __name__ == '__main__':
+    # with psycopg2.connect(database="postgres", user="postgres", password="postgres") as connection:
+
+    db = Database()
+
+    #Добавление клиента
+    # db.add_client('Дмитрий', 'Иванов', 'idvanov@example.com')
+    # db.add_client('Вася', 'Петров', 'pvetrov@example.com')
+    #
+    # #Добавление телефонов для клиента
+    # db.add_phone(1, '123456789')
+    # db.add_phone(1, '987654321')
+    # db.add_phone(2, '5565666777')
+
+    #Поиск клиента
+    # clients = db.find_client('Петр')
+    # print('Найденные клиенты:', clients)
+
+    # Изменение данных клиента
+    # db.update_client(1, 'Игорь','Григорьев','ig@mail.com')
+    #
+    # Удаление телефона
+    # db.delete_phone(1, 1)  # Удаляем первый телефон клиента с id 1
+    #
+    # # Получение телефонов клиента
+    # phones = db.get_client_phones(1)
+    # print('Телефоны клиента 1:', phones)
+
+    # Удаление клиента
+    # db.delete_client(2)
+
+    # Закрытие базы данных
+    db.close()
